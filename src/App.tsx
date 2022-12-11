c5fdbbfbf624a4d79f360c8f66fb4ae7fd9518ef
Bhargav gunreddy <bhargav.gunreddy@procore.com>
2022-12-11 02:17:54 -0500

    always clear interval in useeffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,48 @@
-import React from 'react';
+import React, { useState, useCallback, useEffect, useMemo, useRef } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [timer, setTimer] = useState(10);
+  const [clickCount, setClickCount] = useState(0);
+  const ref = useRef<ReturnType<typeof setTimeout>
+    | null>(null);
+
+
+  const clear = () => clearInterval(ref.current!);
+
+  const increaseCount = useCallback(() => {
+    setClickCount(clickCount + 1);
+  }, [clickCount]);
+
+  useEffect(() => {
+    ref.current = setInterval(() =>
+      setTimer((timer) => timer - 1)
+      , 1000);
+
+    return clear
+  }, []);
+
+  useEffect(() => {
+    if (timer === 0) {
+      clear();
+    }
+  }, [timer])
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <section className="timer">
+        <h4>{timer}</h4>
+
+        <p> Click count {clickCount}</p>
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          {timer > 0 ? <button onClick={increaseCount}>Click</button> : <></>}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </section>
+
     </div>
   );
 }
